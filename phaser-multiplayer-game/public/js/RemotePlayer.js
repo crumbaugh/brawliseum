ad6f204aa2e850055af3c838f5c9a5295d399365
Jason Brillon <Jason62513@gmail.com>
2015-10-18 22:08:05 -0400

    fixed rotation

--- phaser-multiplayer-game/public/js/RemotePlayer.js
+++ phaser-multiplayer-game/public/js/RemotePlayer.js
@@ -9,7 +9,7 @@ var RemotePlayer = function (index, game, player, startX, startY) {
   this.player = player
   this.alive = true
   this.player.speed = 3;
-  this.player = game.add.sprite(x, y, 'dude');
+  this.player = game.add.sprite(x, y, 'enemy');
 
   this.player.name = index.toString()
   this.player.currAttack = 0;
