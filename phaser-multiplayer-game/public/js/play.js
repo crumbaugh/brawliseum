413f906fe09902843a7a3e052d627b06b3762730
Jason Brillon <Jason62513@gmail.com>
2015-10-19 23:47:51 -0400

    your hits

--- phaser-multiplayer-game/public/js/play.js
+++ phaser-multiplayer-game/public/js/play.js
@@ -386,7 +386,8 @@ function playUpdate () {
   scores.push(player.hitcount);
   scores.sort(function(a,b) { return a - b; }).reverse();
 
-  scoreBoard.setText('Top Players:\n1. ' + scores[0] + '\n2.' + scores[1] + '\n3.' + scores[2]);
+  scoreBoard.setText('Most Hits:\n1. ' + scores[0] + '\n2. ' + scores[1] + '\n3. ' + scores[2]
+                    +'\n\n\n\n\n\n\n\n\n\n\nYour Hits: ' + player.hitcount);
 
   oldPlayerRotation = player.rotation;
 
